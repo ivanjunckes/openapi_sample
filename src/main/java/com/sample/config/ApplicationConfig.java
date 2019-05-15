0ce243aa9cc0c75dc73ddfaf6a08434876d38483
ivanjunckes <ijunckes@tomitribe.com>
2019-05-15 09:43:08 -0300

    fix app

--- src/main/java/com/sample/config/ApplicationConfig.java
+++ src/main/java/com/sample/config/ApplicationConfig.java
@@ -6,10 +6,11 @@ import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @OpenAPIDefinition(info = @Info(title = "OpenAPI Sample Project", version = "1.0"),
         security = @SecurityRequirement(name = "oauth2"),
         servers = @Server(url = "/openapi-sample"))
 @ApplicationPath("/api")
-public class ApplicationConfig {
+public class ApplicationConfig extends Application {
 }
